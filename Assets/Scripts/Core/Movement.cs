8c1eca7043a699f796aa56c01891465ce029d85f
Pablo Sánchez <pablosanchez027@hotmail.com>
2019-02-07 13:18:07 -0700

    Practica 2

---
+++ Assets/Scripts/Core/Movement.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Core.Movement
+{
+    public class Movement
+    {
+
+        static Vector2 clampedVelocity;
+
+        /// <summary>
+        /// This function returns a Vector2 Axis of Horixontal and Vertical Inputs.
+        /// </summary>
+        public static Vector2 Axis
+        {
+            get => new Vector2(Input.GetAxis("Horizontal"), 
+                    Input.GetAxis("Vertical"));
+        }
+
+        /// <summary>
+        /// This function returns a Vector2 Axis of Horizontal and Vertical Inputs multiplied by Delta Time.
+        /// </summary>
+        public static Vector2 AxisDelta
+        {
+            get => new Vector2(Input.GetAxis("Horizontal"),
+                    Input.GetAxis("Vertical")) * Time.deltaTime;
+        }
+
+        /// <summary>
+        /// Moves the player in Horizontal multiplied by speed variable.
+        /// </summary>
+        /// <param name="t">Transform component of the player</param>
+        /// <param name="speed">The move speed of the player</param>
+        public static void SimpleMovement(Transform t, float speed)
+        {
+            t.Translate(Vector2.right * Axis.x * speed);
+        }
+
+        /// <summary>
+        /// Moves the player in Horizontal multiplied by speed variable
+        /// and Delta Time.
+        /// </summary>
+        /// <param name="t">Transform component of the player</param>
+        /// <param name="speed">The move speed of the player</param>
+        public static void DeltaMovement(Transform t, float speed)
+        {
+            t.Translate(Vector2.right * AxisDelta.x * speed);
+        }
+
+        /// <summary>
+        /// Returns if player is touching jump button
+        /// </summary>
+        public static bool Btn_Jump
+        {
+            get => Input.GetButtonDown("Jump");
+        }
+
+        /// <summary>
+        /// Makes player jumps with an impulse in rigidbody.
+        /// </summary>
+        /// <param name="rb2d">Rigidbody2D component of player.</param>
+        /// <param name="jumpForce">The magnitude of the jump.</param>
+        public static void PhysicJumpUp(Rigidbody2D rb2d, float jumpForce)
+        {
+            rb2d.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+
+
+        /// <summary>
+        /// Moves the player with physics on the x Axis
+        /// </summary>
+        /// <param name="rb2d"></param>
+        /// <param name="moveSpeed"> The amount of force for movement in X</param>
+        /// <param name="maxSpeed">Max amount of player movement</param>
+        public static void PhysicMovement(Rigidbody2D rb2d, float moveSpeed, float maxSpeed)
+        {
+            rb2d.AddForce(Vector2.right * moveSpeed * Axis.x, ForceMode2D.Impulse);
+
+            clampedVelocity = Vector2.ClampMagnitude(rb2d.velocity, maxSpeed);
+
+            rb2d.velocity = new Vector2(clampedVelocity.x, rb2d.velocity.y);
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Core.Movement;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    Text txtScore;
+    int score;
+
+    [SerializeField]
+    float moveSpeed = 0f;
+
+    [SerializeField, Range(0.1f, 10f)]
+    float maxSpeed = 0f;
+
+    Animator anim;
+    SpriteRenderer spr;
+
+    float moveX;
+
+    Vector2 clampedVelocity;
+
+    [SerializeField]
+    AudioClip sfx_coin;
+
+    AudioSource auds;
+
+    Rigidbody2D rb2d;
+
+    [SerializeField, Range(0.1f, 15f)]
+    float jumpForce = 3f;
+
+    [SerializeField]
+    Color rayColor = Color.magenta;
+
+    [SerializeField, Range(0.0001f, 5f)]
+    float rayLenght = 1f;
+
+    [SerializeField]
+    LayerMask dectectionLayer;
+
+    [SerializeField]
+    Vector2 rayPosition;
+
+   
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        spr = GetComponent<SpriteRenderer>();
+        auds = GetComponent<AudioSource>();
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+
+        moveX = Movement.Axis.x;
+        
+        //Movement.DeltaMovement(transform, moveSpeed);
+
+        anim.SetFloat("MoveX", Mathf.Abs(moveX));
+
+      
+
+        spr.flipX = moveX < 0f ? true : moveX > 0f ? false : spr.flipX;
+
+    }
+
+    private void FixedUpdate()
+    {
+        Movement.PhysicMovement(rb2d, moveSpeed, maxSpeed);
+
+
+        if (Movement.Btn_Jump && Grounding)
+        {
+            Movement.PhysicJumpUp(rb2d, jumpForce);
+        }
+    }
+
+    bool Grounding
+    {
+        get => Physics2D.Raycast(transform.position + (Vector3)rayPosition,
+            Vector2.down, rayLenght, dectectionLayer);
+    }
+
+    public void PlayCoinSFX()
+    {
+        auds.PlayOneShot(sfx_coin, 7f);
+        score++;
+        txtScore.text = "Score: " + score.ToString();
+
+        
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = rayColor;
+        Gizmos.DrawRay(transform.position + (Vector3)rayPosition, Vector2.down * rayLenght);
+    }
+}
